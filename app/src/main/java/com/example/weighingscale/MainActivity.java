c1770ce8b8ee18b7e39c3dc58277272f6f32334e
Dikhi Martin <dikhi.martin@gmail.com>
2024-07-09 14:10:53 +0700

    feat: add state Management

--- app/src/main/java/com/example/weighingscale/MainActivity.java
+++ app/src/main/java/com/example/weighingscale/MainActivity.java
@@ -34,6 +34,7 @@ import android.widget.Toast;
 import com.example.weighingscale.util.BluetoothUtil;
 import com.example.weighingscale.util.ConnectedThread;
 import com.example.weighingscale.ui.shared.SharedViewModel;
+import com.example.weighingscale.state.StateConnecting;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -41,7 +42,7 @@ import java.nio.charset.StandardCharsets;
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_ENABLE_BT = 1;
     public static final int MESSAGE_READ = 2;
-    public static final int CONNECTING_STATUS = 3;
+    public static final int HANDLER_STATUS = 3;
 
     private TextView mReadBuffer;
 
@@ -50,14 +51,19 @@ public class MainActivity extends AppCompatActivity {
     private Handler mHandler;
     private ConnectedThread mConnectedThread;
     private BluetoothSocket mBTSocket = null;
-
     private MenuItem bluetoothMenuItem;
+
     private ActivityMainBinding binding;
+
+    private StateConnecting stateConnecting;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+
+        // Initialize the StateConnecting
+        stateConnecting = new StateConnecting();
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -70,8 +76,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
-        // initView();
 
         // Init Bluetooth
         initBluetooth();
@@ -112,6 +116,8 @@ public class MainActivity extends AppCompatActivity {
         } else if (id == R.id.action_bluetooth) {
             if (!mBluetoothUtil.isBluetoothEnabled()) {
                 bluetoothOn();
+            }else if (stateConnecting.getStatus() == StateConnecting.BLUETOOTH_CONNECTED){
+                return true;
             }else{
                 // Show device list fragment to connect
                 navController.navigate(R.id.navigation_device);
@@ -141,13 +147,8 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize handler for Bluetooth messages
         mHandler = new Handler(Looper.getMainLooper()) {
-
-
             @Override
             public void handleMessage(@NonNull Message msg) {
-                //  Log.d("LOG_SAYA(msg)", String.valueOf(msg.arg1));
-                //  Log.d("LOG_SAYA(what)", String.valueOf(msg.what));
-                handler_indicator_check();
                 switch (msg.what) {
                     case MESSAGE_READ:
                         // TODO : Baca timbangan
@@ -157,18 +158,20 @@ public class MainActivity extends AppCompatActivity {
                         // Log.d("Streams Scale", readMessage);
                         Log.d("LOG_SAYA(readMessage)", readMessage);
                         break;
-                    case CONNECTING_STATUS:
-                        if (msg.arg1 == 1)
-                            //  bluetoothMenuItem.setTitle((CharSequence) msg.obj);
-                            bluetoothMenuItem.setTitle(getString(R.string.connected));
-                        else if (msg.arg1 == 4)
+                    case HANDLER_STATUS:
+                        if (msg.arg1 == 1) {
+                            bluetoothMenuItem.setTitle(getString(R.string.connected) + ": " + msg.obj);
+                            stateConnecting.setStatus(StateConnecting.BLUETOOTH_CONNECTED);
+                        } else if (msg.arg1 == 4) {
                             Toast.makeText(getApplicationContext(), getString(R.string.device_already_connected), Toast.LENGTH_SHORT).show();
-                        else
+                        } else {
                             Toast.makeText(getApplicationContext(), getString(R.string.connection_fail), Toast.LENGTH_SHORT).show();
+                        }
                         break;
                 }
             }
         };
+
 
         // Check if Bluetooth is supported on this device
         if (mBluetoothUtil.getAdapter() == null) {
@@ -191,29 +194,11 @@ public class MainActivity extends AppCompatActivity {
     private void bluetoothOn() {
         if (!mBluetoothUtil.isBluetoothEnabled()) {
             mBluetoothUtil.enableBluetooth(MainActivity.this, REQUEST_ENABLE_BT);
-            //  mBluetoothStatus.setText(getString(R.string.BTEnable));
             Toast.makeText(getApplicationContext(), getString(R.string.bluetooth_turn_on), Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(getApplicationContext(), getString(R.string.bluetooth_already_on), Toast.LENGTH_SHORT).show();
         }
     }
-
-    // START : BLUETOOTH Select Device
-    //    private final AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
-    //        @Override
-    //        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    //            if (!mBluetoothUtil.getAdapter().isEnabled()) {
-    //                Toast.makeText(getBaseContext(), getString(R.string.BTnotOn), Toast.LENGTH_SHORT).show();
-    //                return;
-    //            }
-    //            mBluetoothStatus.setText(getString(R.string.cConnet));
-    //            String info = ((TextView) view).getText().toString();
-    //            final String address = info.substring(info.length() - 17);
-    //            final String name = info.substring(0, info.length() - 17);
-    //            connectToDevice(address, name);
-    //        }
-    //    };
-    // END : BLUETOOTH MODULE
 
     public void connectToDevice(String address, String name) {
         bluetoothMenuItem.setTitle(R.string.connecting);
@@ -235,7 +220,7 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         fail = true;
                         mBTSocket.close();
-                        mHandler.obtainMessage(CONNECTING_STATUS, -1, -1).sendToTarget();
+                        mHandler.obtainMessage(HANDLER_STATUS, -1, -1).sendToTarget();
                     } catch (IOException e2) {
                         runOnUiThread(() -> Toast.makeText(getBaseContext(), getString(R.string.ErrSockCrea), Toast.LENGTH_SHORT).show());
                     }
@@ -243,7 +228,7 @@ public class MainActivity extends AppCompatActivity {
                 if (!fail) {
                     mConnectedThread = new ConnectedThread(mBTSocket, mHandler);
                     mConnectedThread.start();
-                    mHandler.obtainMessage(CONNECTING_STATUS, 1, -1, name).sendToTarget();
+                    mHandler.obtainMessage(HANDLER_STATUS, 1, -1, name).sendToTarget();
                 }
             }
         }.start();
@@ -252,18 +237,34 @@ public class MainActivity extends AppCompatActivity {
     private void set_indicator_bt_enable(){
         bluetoothMenuItem.setIcon(null);
         bluetoothMenuItem.setTitle(R.string.connect_to_scale);
+        stateConnecting.setStatus(StateConnecting.BLUETOOTH_ENABLED);
     }
 
     private void set_indicator_bt_disable(){
         bluetoothMenuItem.setIcon(R.drawable.ic_bluetooth_disabled_24dp);
-    }
-
-    private void handler_indicator_check(){
-          Log.d("LOG_SAYA(msg)", "Runtime handler_indicator_check()");
+        stateConnecting.setStatus(StateConnecting.BLUETOOTH_DISABLED);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    // START : BLUETOOTH Select Device
+    //    private final AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+    //        @Override
+    //        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    //            if (!mBluetoothUtil.getAdapter().isEnabled()) {
+    //                Toast.makeText(getBaseContext(), getString(R.string.BTnotOn), Toast.LENGTH_SHORT).show();
+    //                return;
+    //            }
+    //            mBluetoothStatus.setText(getString(R.string.cConnet));
+    //            String info = ((TextView) view).getText().toString();
+    //            final String address = info.substring(info.length() - 17);
+    //            final String name = info.substring(0, info.length() - 17);
+    //            connectToDevice(address, name);
+    //        }
+    //    };
+    // END : BLUETOOTH MODULE
+
 }
