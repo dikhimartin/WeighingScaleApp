b3a0f30b1b57ba7fc872a93dc537c51a2c7d9583
Dikhi Martin <dikhi.martin@gmail.com>
2024-07-09 15:46:27 +0700

    wip: preparing batch save

--- app/src/main/java/com/example/weighingscale/ui/batch/BatchFragment.java
+++ app/src/main/java/com/example/weighingscale/ui/batch/BatchFragment.java
@@ -1,4 +1,4 @@
-package com.example.weighingscale.ui.notifications;
+package com.example.weighingscale.ui.batch;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,21 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.weighingscale.databinding.FragmentNotificationsBinding;
+import com.example.weighingscale.databinding.FragmentBatchBinding;
 
-public class NotificationsFragment extends Fragment {
+public class BatchFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentBatchBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        BatchViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(BatchViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentBatchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
+        final TextView textView = binding.textBatch;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
